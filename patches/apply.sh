bba0938327377bded99db55975927cdfd2002c34
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2017-12-28 23:36:49 +0530

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,4 +2,8 @@
 
 patch -p1 < ../../device/LAVA/IRISX8/patches/frameworks_av.patch
 
+cd ../..
+cd apps/Settings
+patch -p1 -b < ../../../device/LAVA/IRISX8/patches/dev_info.patch
+
 echo Successfuly patched! Go ahead and start building!
