0cd8eca495f560ffe7124198fec6ff59340e95c6
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2018-01-05 22:51:08 +0530

    Delete apply.sh

--- patches/apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-patch -p1 < ../../device/LAVA/IRISX8/patches/frameworks_av.patch
-
-cd ../..
-cd apps/Settings
-patch -p1 -b < ../../../device/LAVA/IRISX8/patches/dev_info.patch
-
-echo Successfuly patched! Go ahead and start building!
