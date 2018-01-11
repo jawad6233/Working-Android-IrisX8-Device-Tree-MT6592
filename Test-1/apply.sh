2538127fe0204d258733f3bf20848ca88c5d0efe
Raj Kumar <33460256+Attached2Android@users.noreply.github.com>
2018-01-11 13:06:15 +0530

    Update apply.sh

--- Test-1/apply.sh
+++ Test-1/apply.sh
@@ -2,51 +2,51 @@
 
  cd ../../../..
  cd system/core
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_android_system_core_MTK.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_android_system_core_MTK.patch
  
  cd ..
  cd bt
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_system_bt.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_system_bt.patch
  
  cd ..
  cd netd
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_system_netd.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_system_netd.patch
  
  cd ..
  cd sepolicy
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_system_sepolicy_MTK.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_system_sepolicy_MTK.patch
  
  cd ../..
  cd frameworks/av
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_frameworks_av.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_frameworks_av.patch
  
  cd ..
  cd base
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_frameworks_base.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_frameworks_base.patch
  
  cd ..
  cd native
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_frameworks_native_MTK.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_frameworks_native_MTK.patch
  
  cd ..
  cd opt/telephony
- patch -p1 < ../../../device/LAVA/IRISX8/patches/android_frameworks_opt_telephony.patch
+ patch -p1 < ../../../device/LAVA/IRISX8/Test-1/android_frameworks_opt_telephony.patch
  
  cd ../../..
  cd external/icu
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_external_icu.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_external_icu.patch
  
  cd ..
  cd wpa_supplicant_8
- patch -p1 < ../../device/LAVA/IRISX8/patches/android_external_wpa_supplicant_8.patch
+ patch -p1 < ../../device/LAVA/IRISX8/Test-1/android_external_wpa_supplicant_8.patch
  
  cd ../../..
  cd vendor/codeaurora/telephony
- patch -p1 -p1 < ../../../device/LAVA/IRISX8/patches/android_vendor_codeaurora_telephony.patch
+ patch -p1 -p1 < ../../../device/LAVA/IRISX8/Test-1/android_vendor_codeaurora_telephony.patch
  
  cd ../../..
  cd packages/services/Telephony
- patch -p1 -p1 < ../../../device/LAVA/IRISX8/patches/android_packages_services_Telephony.patch
+ patch -p1 -p1 < ../../../device/LAVA/IRISX8/Test-1/android_packages_services_Telephony.patch
  cd ../../..
  
  echo Successfuly patched!
